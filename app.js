f502ece139cef114f2098b5cd0b8c1e3aec79a97
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-16 18:28:39 -0500

    captured user input on command line

---
+++ app.js
@@ -0,0 +1,15 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.lenth; i++) {
+        console.log(profileDataArr);
+    }
+
+    console.log("===============");
+
+    // Is the same as this ...
+    profileDataArr.forEach(profileItem => console.log(profileItem))
+  };
+  
+  printProfileData(profileDataArgs);
