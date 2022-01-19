7333f8f66aace2582d21ddc9069a0b63c46d8ceb
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-19 00:06:27 -0500

    added inquirer and got it to work

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+   .prompt([
+     {
+       type: 'input',
+       name: 'name',
+       message: 'What is your name?'
+     }
+   ])
 
-const [name, github] = profileDataArgs;
+   .then(answers => console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
 
   
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 // const printProfileData = profileDataArr => {
